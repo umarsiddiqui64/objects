c6da805be3aff78e8e6fb5ad0b7c519e73c83bd8
Umar Siddiqui <umarsiddiqui64@gmail.com>
2024-12-04 22:12:39 +0530

    Initial commit: Added solution for Senior Citizen Discount Eligibility

---
+++ solution.js
@@ -0,0 +1,10 @@
+let age = 65; 
+
+let result = (age <= 0) 
+    ? "Invalid Age" 
+    : (age >= 60) 
+        ? "Eligible for Senior Discount" 
+        : "Not Eligible for Senior Discount";
+
+
+console.log(result);
